5848de7a0640cefe25d1e7e379e6c7d7f22d6dd1
jimlin <kangxianlin@gmail.com>
2019-03-22 15:37:57 +1300

    Added AsyncAndAwaitExample

---
+++ AsyncAndAwaitExample/Example3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = new Task(CallMethod);
+            task.Start();
+            task.Wait();
+            Console.ReadLine();
+        }
+        static async void CallMethod()
+        {
+            string filePath = AppDomain.CurrentDomain.BaseDirectory+"note.txt";
+            Task<int> task = ReadFile(filePath);
+
+            Console.WriteLine("Other Work 1");
+            Console.WriteLine("Other Work 2");
+            Console.WriteLine("Other Work 3");
+
+            int length = await task;
+            Console.WriteLine("Total length:"+length);
+            Console.WriteLine("After work 1");
+            Console.WriteLine("After work 2");
+        }
+        static async Task<int> ReadFile(string filePath)
+        {
+            int length = 0;
+            Console.WriteLine("File reading is starting");
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string s = await reader.ReadToEndAsync();
+                length = s.Length;
+            }
+            Console.WriteLine("File reading is completed");
+            return length;
+        }
+    }
+}
