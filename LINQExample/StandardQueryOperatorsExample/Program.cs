f0a36d992d6aaaa8e30710201e16216368004e56
jimlin <kangxianlin@gmail.com>
2019-04-16 20:41:42 +1200

    Learned Tuple

--- LINQExample/StandardQueryOperatorsExample/Program.cs
+++ LINQExample/StandardQueryOperatorsExample/Program.cs
@@ -11,6 +11,26 @@ namespace StandardQueryOperatorsExample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("=========Tuple==========");
+
+            Tuple<int, string, string> person = new Tuple<int, string, string>(1, "Jim", "Lin");
+
+            Console.WriteLine(person.Item1);
+            Console.WriteLine(person.Item2);
+            Console.WriteLine(person.Item3);
+
+
+            var numbers = Tuple.Create("One", 2, 3, "Four", 5, "Six", 7, 8);
+
+            Console.WriteLine(numbers.Item1); // returns "One"
+            Console.WriteLine(numbers.Item2); // returns 2
+            Console.WriteLine(numbers.Item3); // returns 3
+            Console.WriteLine(numbers.Item4); // returns "Four"
+            Console.WriteLine(numbers.Item5); // returns 5
+            Console.WriteLine(numbers.Item6); // returns "Six"
+            Console.WriteLine(numbers.Item7); // returns 7
+            Console.WriteLine(numbers.Rest); // returns (8)
+            Console.WriteLine(numbers.Rest.Item1); // returns 8
 
             // Student collection
             IList<Student> studentList = new List<Student>() {
